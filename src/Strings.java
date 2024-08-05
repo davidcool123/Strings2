17ef8d858e0db2a6b6c4796e2a7cc7b7df4f6103
David Peterson <peterdav000@warren.k12.in.us>
2024-08-05 13:39:16 -0400

    Initial commit

---
+++ src/Strings.java
@@ -0,0 +1,10 @@
+public class Strings {
+
+        public static void main(String[] args){
+        byte  byteValue = 10;
+        short shortValue = 5;
+        int intValue = 3;
+        long myLong = 800000 - 5 + (byteValue + shortValue + intValue);
+        System.out.println(longTotal);
+        }
+}
